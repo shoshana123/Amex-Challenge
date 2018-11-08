190bfd16b8f808f2819b5fcf8330a7258611cd89
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-11-08 16:57:21 -0500

    update to retrieve film data

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import axios from 'axios'
 import {Navbar} from './components'
 import Routes from './routes'
-import Characters from './data/characters.json'
+import CharactersData from './data/characters.json'
 
 class App extends Component {
   constructor(){
@@ -13,36 +13,53 @@ class App extends Component {
   }
   async componentDidMount() {
     // console.log('characters',Characters.characters)
-    let characterArray = []
-    Characters.characters.map(async character=>{
+    let characterArrayPromises = CharactersData.characters.map(character => {
       try{
-        const {data} = await axios.get(character.url)
-        try{
-          await data.films.map(async film => {
-            const filmObject = await axios.get(film)
-            // console.log('filmObj',filmObject.data)
-            return filmObject.data
-          })
-          await console.log('data',data.films)
-        } catch (err){
-          console.error(err)
-        }
-        await console.log('fiml',data.films)
-        return {
-          name: character.name,
-          data: characterArray.push(data)
-        }
-      } catch (err) {
-        console.error(err)
+        return axios.get(character.url).catch(err=>err)
+      } catch (err){
+        return null
       }
 
     })
-    // console.log('character,',characterArray)
-    // const {data} = await axios.get('https://swapi.co/api/people')
-    // console.log('data',data.results)
+
+    let characterArray = await Promise.all(characterArrayPromises)
+    characterArray = characterArray.filter(character=>!!character.data===true)
+
+    // characterArray.map(character => character.data)
+    for(let i = 0; i < characterArray.length; i++) {
+      console.log('character',characterArray[i])
+      for (let j = 0; j < characterArray[i].data.films.length; j++) {
+        let filmObj = await axios.get(characterArray[i].data.films[j])
+        console.log('filmObj',filmObj)
+      }
+    }
+      // return (character.films.map(async film => {
+      //   const {data} = await axios.get(film)
+      //   console.log('data',data)
+      //   return data
+      // }))
+
+    this.setState({
+      characters: characterArray
+    })
+
+    //  console.log('charactersdata',state)
+    // try{
+    //   await data.films.map(async film => {
+    //     const filmObject = await axios.get(film)
+    //     // console.log('filmObj',filmObject.data)
+    //     return filmObject.data
+    //   })
+    //   console.log('data',data.films)
+    // } catch (err){
+    //   console.error(err)
+    // }
   }
 
   render() {
+
+    console.log('state',this.state.characters)
+
     return (
       <div>
         <Navbar />
