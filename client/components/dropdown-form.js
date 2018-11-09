694a1b5976ed4e6d1311398a04685dbd4f9a370b
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-11-09 12:45:56 -0500

    Include character form dropdown and ability to get films data per selected character

--- client/components/dropdown-form.js
+++ client/components/dropdown-form.js
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-export default (props)=> {
-const {characters} = props
+export default class DropDownForm extends Component {
+  constructor(props){
+    super()
+    this.state= {
+      selectedCharacter: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      selectedCharacter: event.target.value
+    })
+    console.log('stateCheck!',this.state)
+  }
+  async handleSubmit(event) {
+    event.preventDefault()
+    const selectedCharacterData = this.props.characters.filter(character=>character.name===this.state.selectedCharacter)[0]
+    console.log('test',selectedCharacterData)
+    let selectedCharacterFilmsArrayPromises = selectedCharacterData.films.map(film => {
+      try {
+        return axios.get(film)
+        .catch(err=>err)
+      } catch (err) {
+        return null
+      }
+    })
+    let filmsData = await Promise.all(selectedCharacterFilmsArrayPromises)
+    filmsData = filmsData.map(film=>film.data)
+    this.setState({
+      selectedCharacter: ''
+    })
+
+    console.log('TEST',filmsData)
+  }
+  render() {
+
+
+const {characters} = this.props
 return (
-    <form>
+    <form onSubmit={this.handleSubmit}>
     <label>
       Pick Your Favorite Star Wars Character:
-    <select>
+    <select value={this.state.selectedCharacter} onChange={this.handleChange}>
       <option value='--'>--</option>
       {characters.map((character, index) => {
         return (
-            <option value={character.name} key={index}>{character.name}</option>
+            <option value={character.name} key={index} >{character.name}</option>
         )
       })}
     </select>
     </label>
+    <input type="submit" value="Submit" />
     </form>
 )
+}
 }
