4dda3cd9b390d99ca5c83b901614c7db19b65f1a
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-11-11 03:26:41 -0500

    Fix the release date format for month, day, year

--- client/components/movieRow.js
+++ client/components/movieRow.js
@@ -2,12 +2,33 @@ import React from 'react'
 
 export default (props) => {
   const {movie} = props
+
   return (
     <tr>
       <td>{movie.title}</td>
       <td>{movie.director}</td>
       <td>{movie.producer}</td>
-      <td>{movie.release_date}</td>
+      <td>{formatDate(movie.release_date)}</td>
     </tr>
   )
+}
+
+
+function formatDate (movieData) {
+  let monthConverter = {
+    '01' : 'January',
+    '02' : 'February',
+    '03' : 'March',
+    '04' : 'April',
+    '05' : 'May',
+    '06' : 'June',
+    '07' : 'July',
+    '08' : 'August',
+    '09' : 'September',
+    '10' : 'October',
+    '11' : 'November',
+    '12' : 'December'
+  }
+  const movieDataArray = movieData.split('-')
+  return `${monthConverter[movieDataArray[1]]} ${movieDataArray[2]}, ${movieDataArray[0]}`
 }
