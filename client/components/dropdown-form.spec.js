0e5d03b3594e9904f76e91023e30afe782771dc7
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-11-12 20:29:04 -0500

    update to include tests

--- client/components/dropdown-form.spec.js
+++ client/components/dropdown-form.spec.js
@@ -5,13 +5,15 @@ import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import DropDownForm from './dropdown-form'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
 describe('DropDown Form', () => {
   let DropDown
-  let data = [
+  let fakeData = [
     {
       "name": "Luke Skywalker",
       "url": "https://swapi.co/api/people/1/"
@@ -21,8 +23,11 @@ describe('DropDown Form', () => {
       "url": "https://swapi.co/api/people/4/"
     }]
 
+    let fakeDataArray = Object.values(fakeData[0])
+    console.log('check',fakeDataArray)
+
   beforeEach(() => {
-    DropDown = shallow(<DropDownForm characters={data} />)
+    DropDown = shallow(<DropDownForm characters={fakeData} />)
   })
 
   it('renders all of the character options including the initial blank option', () => {
@@ -30,6 +35,17 @@ describe('DropDown Form', () => {
   })
 
   // it('renders the character name', () => {
-  //   expect(DropDown.find('.Select').value).to.be.equal('R2-D2')
+  //   console.log('!!!',DropDown.find('select').props().children)
+  //   expect(DropDown.find('option').value).to.be.equal('R2-D2')
+  // })
+
+  // it('makes an axios request to get the characters', () => {
+  //   const mockAxios = new MockAdapter(axios)
+  //   mockAxios.onGet('https://swapi.co/api/people/1/').replyOnce(200, fakeData[0])
+  //   console.log('test!!',expect(axios.get('https://swapi.co/api/people/1/')
+  //   .then((response) => response.data.name)
+  //   .catch((err) => console.error(err))))
+  //   expect(axios.get('https://swapi.co/api/people/1/')
+  //     .then((response) => response.data.name)).to.be.equal('Luke Skywalker')
   // })
 })
